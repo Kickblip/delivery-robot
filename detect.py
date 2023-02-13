e86e491c36e014f59c51748f4b7322c1cb82279b
Kickblip <wyatt.zilker@gmail.com>
2023-02-13 15:27:16 -0600

    person detection

--- detect.py
+++ detect.py
@@ -85,6 +85,31 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
     # Draw keypoints and edges on input image
     image = utils.visualize(image, detection_result)
 
+    # iterate through detectionresult and check if there is a category_name = 'person'
+
+
+    # for detection in detection_result.detections:
+    #   if detection.categories[0].category_name == 'person':
+    #     print('person detected')
+    #     break
+
+    if len(detection_result.detections) > 0:
+      if detection_result.detections[0].categories[0].category_name == 'person':
+        print('person detected')
+    # print(detection_result.detections[0].categories[0].category_name)
+
+
+
+    """
+    DetectionResult(
+      detections=[Detection(bounding_box=BoundingBox(origin_x=-3, origin_y=226, width=648, height=247), 
+      categories=[Category(index=64, score=0.43359375, display_name='', category_name='bed')])]
+      
+      )
+
+    """
+
+
     print(detection_result);
 
     # Calculate the FPS
