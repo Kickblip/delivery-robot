0240dbe1c2f3396c85f7e51602a9f6f155552595
Kickblip <wyatt.zilker@gmail.com>
2023-03-22 09:49:05 -0500

    test3

--- handler.py
+++ handler.py
@@ -1,16 +1,30 @@
 import time
 import RPi.GPIO as GPIO
 
+runFunction = True
+
 def handleObstacle (detection):
     print(f"obstacle detected - score: {detection.categories[0].score}")
 
+    if (runFunction):
+        runFunction = False
+
+        # GPIO.output(17, GPIO.HIGH)
+        print("LED on")
+        GPIO.output(17,GPIO.HIGH)
+        time.sleep(1)
+        print("LED off")
+        GPIO.output(17,GPIO.LOW)
+
+        runFunction = True
+
+    # GPIO.output(17,GPIO.HIGH)
 
 
 
 
-    GPIO.output(17, GPIO.HIGH)
 
-    # physical pin 11 - signal - GPIO 18
+    # physical pin 11 - signal - GPIO 17
     # ground 6
     # 5V power 2
 
